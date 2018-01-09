7f5c2341397d8f2b5efc9bf7361afcf04c0f0d97
zmy <hzzmyuan@tairanchina.com>
2018-01-09 19:26:54 +0800

    feat: rate

---
+++ MachineLearning/LinearRegression/rate.py
@@ -0,0 +1,58 @@
+"""
+线性回归
+"""
+import pandas as pd
+from sklearn import datasets, linear_model
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def get_data(file_name):
+    """
+    读取csv文件并转换返回元组
+    """
+    data = pd.read_csv(file_name)
+    x_parameter = []
+    y_parameter = []
+    for single_square_feet, single_price in zip(data['square_feet'],
+                                                data['price']):
+        x_parameter.append([float(single_square_feet)])
+        y_parameter.append(float(single_price))
+    return x_parameter, y_parameter
+
+
+def liner_modal_main(x_parameter, y_parameter, predice_value):
+    """
+    把数据拟合为线性回归模型
+    """
+    regr = linear_model.LinearRegression()  # 创建线性模型
+    regr.fit(x_parameter, y_parameter)  # 训练
+    predice_outcome = regr.predict(predice_value)
+    predictions = {}
+    predictions['intercept'] = regr.intercept_
+    predictions['coefficient'] = regr.coef_
+    predictions['predicted_value'] = predice_outcome
+    return predictions
+
+
+def show_linear_line(x_parameter, y_parameter):
+    regr = linear_model.LinearRegression()  # 创建线性模型
+    regr.fit(x_parameter, y_parameter)  # 训练
+    plt.scatter(x_parameter, y_parameter, color='blue')
+    plt.plot(x_parameter, regr.predict(x_parameter), color='red', linewidth=4)
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
+
+
+def main():
+    x, y = get_data('./data/rate.csv')
+    show_linear_line(x, y)
+    predict_value = 700  # 预测的平方英尺
+    result = liner_modal_main(x, y, predict_value)
+    print("Intercept value ", result['intercept'])
+    print("coefficient", result['coefficient'])
+    print("Predicted value: ", result['predicted_value'])
+
+
+main()
